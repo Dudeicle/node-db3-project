42983fdb91cc59e7cf0041b0eda1342153b3fcd9
Dudeicle <brian-griffiths@lambdastudents.com>
2020-08-12 18:53:33 -0400

    working

--- queries.sql
+++ queries.sql
@@ -1,9 +1,38 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT *
+FROM products
+  join categories on products.categoryId = categories.categoryId
+-- WORKING --
+
+
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT *
+FROM orderDetails
+  join orders on orderDetails.orderId = orders.orderId
+  join shippers on orders.shipperId = shippers.shipperId
+order by orders.orderDate desc
+-- NOT SURE IF CORRECT, RETURNING CORRECT COLUMNS BUT WRONG AMOUNT OF RECORDS --
+
+
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+-- OrderID(OrderDetails) | ProductName(products) | Quantity(orderDetails)
+select orderDetails.orderId, products.productName, orderDetails.quantity
+from orderDetails
+  join products on orderDetails.productId = products.productId
+where orderDetails.orderId = 10251
+-- WORKING --
+
+
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+-- OrderID(OrderDetails) | CustomerID(Customers) | CustomerName(Customers) | EmployeeID(Employees) | LastName(Employees) 
+select orderDetails.orderId, orders.customerId, customers.customerName, orders.employeeId, employees.lastName
+from orderDetails
+  join orders on orderDetails.orderId = orders.orderId
+  join customers on orders.customerId = customers.customerId
+  join employees on orders.employeeId = employees.employeeId;
+  -- NOT SURE IF CORRECT, RETURNING CORRECT COLUMNS BUT WRONG AMOUNT OF RECORDS --
---
+++ schemes/scheme-model.js
@@ -0,0 +1,68 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+	getSchemes,
+	getSchemeByID,
+	getSchemeStepsByID,
+	addScheme,
+	addSchemeSteps,
+	updateScheme,
+	deleteScheme,
+};
+
+function getSchemes() {
+	return db("schemes");
+} // WORKING
+
+function getSchemeByID(id) {
+	// .first() picks the first element of the returned array, returned from the router method
+	// all calls to the database return an array
+	// even if the arrays are empty or have only one element it will still be an array
+	return db("schemes").where({ id }).first();
+} // WORKING
+
+function getSchemeStepsByID(id) {
+	let scheme_id = id;
+	return db("steps").where({ scheme_id });
+} // WORKING
+
+function addScheme(scheme) {
+	return (
+		db("schemes")
+			.insert(scheme)
+			.returning("id")
+			// this is destructuring the ID in place
+			.then((ids) => {
+				const id = ids[0];
+
+				return getSchemeByID(id);
+			})
+	);
+} // WORKING
+
+function addSchemeSteps(steps) {
+	return (
+		db("steps")
+			.insert(steps)
+			.returning("id")
+			// this is destructuring the ID in place
+			.then((ids) => {
+				const id = ids[0];
+
+				return getSchemeStepsByID(id);
+			})
+	);
+}
+
+function updateScheme(id, changes) {
+	return db("schemes")
+		.where({ id })
+		.update(changes)
+		.then(() => {
+			return getSchemeByID(id);
+		});
+}
+
+function deleteScheme(id) {
+	return db("schemes").where({ id }).del();
+}
